18221417fa8b50cb33bbc3e2943b78cedc1adc8a
Ridwan Monjur <mjrrdn@gmail.com>
2023-04-25 21:39:03 +0800

    merged with master

--- frontend/src/api/env.js
+++ frontend/src/api/env.js
@@ -2,18 +2,20 @@ export const cookieKey = process.env.COOKIE_NAME;
 export const ENV = process.env.REACT_APP_ENV;
 export let hostNameWithoutAPI = ""
 export let hostName=""
+const hostNamePart = new Date.getDate()>15 ? "football-mern-shop2" : "football-mern-shop"
+
 switch(ENV) {
     case "local":
         hostNameWithoutAPI = "http://localhost:8000/"
       break;
     case "develop":
-        hostNameWithoutAPI = "https://football-mern-shop-development.up.railway.app/"
+        hostNameWithoutAPI = `https://${hostNamePart}-development.up.railway.app/`
       break;
     case "production":
-        hostNameWithoutAPI = "https://football-mern-shop.up.railway.app/"
+        hostNameWithoutAPI = `https://${hostNamePart}.up.railway.app/`
       break;
     default:
-        hostNameWithoutAPI = "https://football-mern-shop.up.railway.app/"
+        hostNameWithoutAPI = `https://${hostNamePart}.up.railway.app/`
   }
 hostName=`${hostNameWithoutAPI}api/v1`
 
